22ee300673b1f9faf5846ee252450c4dd4ca4663
Andreas Manuel Korn <50735624+AMKorn@users.noreply.github.com>
2022-10-20 17:08:14 +0200

    beautification and flex preparation

--- lenguaG/src/lenguag/LenguaG.java
+++ lenguaG/src/lenguag/LenguaG.java
@@ -1,6 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+/**
+ * Asignatura: 21780 - Compiladores
+ * Miembros:
+ * 	- Román Colom, Marc
+ * 	- Korn, Andreas Manuel
+ * 	- Vilella Candía, Joan 
  */
 package lenguag;
 
