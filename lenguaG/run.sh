30f16fa8a4d457ff771161b86a870225ed80e5e0
Andreas Manuel Korn <50735624+AMKorn@users.noreply.github.com>
2022-10-26 13:35:32 +0200

    Improved run.sh

--- lenguaG/run.sh
+++ lenguaG/run.sh
@@ -1,4 +1,30 @@
-cd src/
-javac lenguag/LenguaG.java
-java lenguag.LenguaG ../$1
+if [ $# -eq 0 ]
+then
+    echo "Unexpected format. Expected format:"
+    echo $0" [--jflex] file"
+    exit 1
+elif [ $# -eq 1 ]
+then
+    file=$1
+    mode=0
+elif [ $# -eq 2 ]
+then
+    file=$2
+    mode=$1
+fi
+
+cd src/ 
+
+if [ $mode = "--jflex" ]
+then
+    echo "
+    **** Running jflex ****
+    "
+    jflex lenguag/lexic/lenguaG.flex
+fi
+javac lenguag/*.java
+echo "
+    ***** Program output *****
+"
+java lenguag.LenguaG ../$file
 cd ..
